eac67987fdb2292aa0cc415714b6a873bfb3cbac
Justin <165220142+DeltaV09@users.noreply.github.com>
2024-06-20 18:37:15 -0500

    move container object to global scope

--- javascript.js
+++ javascript.js
@@ -1,14 +1,16 @@
 //etch-a-sketch
+const container = document.querySelector(".container");
 
+
+//make grid for the play area, take user input or default to 16x16
 function makeBoxes(dimension = 16) {
-    const container = document.querySelector(".container");
     for (let i = 0; i < dimension; i++) {
-        let newRow = document.createElement("div");
+        const newRow = document.createElement("div");
         newRow.id = "r" + i;
         newRow.setAttribute("style","display: flex; flex: 1;");
         container.appendChild(newRow);
         for(let j = 0; j < dimension; j++) {
-            let newDiv = document.createElement("div");
+            const newDiv = document.createElement("div");
             newDiv.id = i + "," + j;
             newDiv.setAttribute("style","flex: 1; border-style: solid; border-color: black");
             newRow.appendChild(newDiv);            
