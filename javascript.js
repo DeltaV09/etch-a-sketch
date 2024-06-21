d291aca601f016ddf7791e0a52c6250d4c86b293
Justin <165220142+DeltaV09@users.noreply.github.com>
2024-06-20 19:34:19 -0500

    Add prompt to let user choose number of boxes Also verify user input to only accept a range of values

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 //etch-a-sketch
+const start = document.querySelector("#start");
 const container = document.querySelector(".container");
 
 
 //make grid for the play area, take user input or default to 16x16
 function makeBoxes(dimension = 16) {
+    container.replaceChildren();
+    container.style.background = "white";
     for (let i = 0; i < dimension; i++) {
         const newRow = document.createElement("div");
         newRow.id = "r" + i;
@@ -18,7 +21,13 @@ function makeBoxes(dimension = 16) {
     }
 }
 
-makeBoxes(99);
+start.addEventListener("click", (event) => {
+    let dimension = prompt("Enter an integer between 1 and 100");
+    while (dimension < 1 || dimension > 100) {
+        dimension = prompt("Please only enter integers between 1 and 100");
+    }
+    makeBoxes(dimension);
+})
 
 container.addEventListener("mouseover", (event) => {
     let target = event.target;
